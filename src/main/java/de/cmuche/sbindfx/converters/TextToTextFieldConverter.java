c396a849f81d886431004dce0aa1b839cf79ba91
Christoph Muche <christoph@muche.info>
2020-02-08 15:29:25 +0100

    fix

--- src/main/java/de/cmuche/sbindfx/converters/TextToTextFieldConverter.java
+++ src/main/java/de/cmuche/sbindfx/converters/TextToTextFieldConverter.java
@@ -1,5 +1,19 @@
 package de.cmuche.sbindfx.converters;
 
-public class TextToTextFieldConverter
+import de.cmuche.sbindfx.SbindConverter;
+import javafx.scene.control.TextField;
+
+public class TextToTextFieldConverter implements SbindConverter<String, TextField>
 {
+  @Override
+  public TextField convert(String s)
+  {
+    return new TextField(s);
+  }
+
+  @Override
+  public String back(TextField textField)
+  {
+    return textField.getText();
+  }
 }
